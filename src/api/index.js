2191c79c46b5425cd0f7f9fd09d7ef292927b99f
Edwin J. Páez <edwinjpr11@gmail.com>
2023-11-17 17:17:27 -0500

    feat: make api request

---
+++ src/api/index.js
@@ -0,0 +1,21 @@
+const BASE_URL = 'https://data2.talentpitch.co/api'
+
+const getOffers = async () => {
+  try {
+    const res = await fetch(
+      `${BASE_URL}/homeservice/categories/d/challenges/trending_challenges/unique/null?limit=10`,
+      {
+        method: 'GET',
+        'Content-Type': 'application/json'
+      }
+    )
+    const { data } = await res.json()
+    console.log('data', data)
+    return data
+  } catch (error) {
+    console.error('Error fetching offers', error)
+    return []
+  }
+}
+
+export { getOffers }
