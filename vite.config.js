aed78307fc5c8d77f8fc6994d1a1a56c19a48a2e
Edwin J. Páez <edwinjpr11@gmail.com>
2023-12-13 15:02:56 -0500

    feat: update base href

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/Talentpitch/',
+  base: '/talentpitch/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
